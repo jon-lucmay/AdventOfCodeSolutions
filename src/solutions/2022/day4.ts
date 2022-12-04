fb6ead2e769e74377c3a6c2b7225509351b2f562
Jon-Luc May <a981832@bestbuy.com>
2022-12-04 00:18:37 -0600

    Add day 4 solution

--- src/solutions/2022/day4.ts
+++ src/solutions/2022/day4.ts
@@ -1,1 +1,29 @@
-export const day4 = () => {};
+export const day4 = (arrayOfData: string[]) => {
+  let total = 0;
+  let total2 = 0;
+
+  const nestedElves = arrayOfData.map(elves =>
+    elves.split(',').map(elf => elf.split('-').map(Number))
+  );
+
+  for (const elv of nestedElves) {
+    if (
+      (elv[0][0] >= elv[1][0] && elv[0][1] <= elv[1][1]) ||
+      (elv[0][0] <= elv[1][0] && elv[0][1] >= elv[1][1])
+    ) {
+      total++;
+    }
+
+    if (
+      (elv[0][0] >= elv[1][0] && elv[0][0] <= elv[1][1]) ||
+      (elv[0][1] >= elv[1][0] && elv[0][1] <= elv[1][1]) ||
+      (elv[1][1] >= elv[0][0] && elv[1][1] <= elv[0][1]) ||
+      (elv[1][0] >= elv[0][0] && elv[1][0] <= elv[0][1])
+    ) {
+      total2++;
+    }
+  }
+
+  console.log('part1: ' + total);
+  console.log('part2: ' + total2);
+};
